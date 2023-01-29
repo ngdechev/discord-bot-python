0eb4fa8e8c64f73af50b76df522c21d948afaa62
N. Dechev <n.dechev@abv.bg>
2023-01-29 20:36:37 +0200

    Edited unban command
    Added an error message when a user is not banned.

--- cogs/unban.py
+++ cogs/unban.py
@@ -17,23 +17,22 @@ class Unban(commands.Cog):
     async def unban(self, message, member_id):
         try:
             user = discord.Object(id=member_id)
+            channel = discord.utils.get(message.guild.channels, name="kick-ban")
             member = await self.client.fetch_user(member_id)
 
             await message.guild.unban(user)
-
-            channel = discord.utils.get(message.guild.channels, name="kick-ban")
-
             unban_embed = discord.Embed(colour=discord.Color.green())
             unban_embed.set_author(name=f"{self.client.user.display_name} || Member Unban",
-                                   icon_url=f"{self.client.user.display_avatar}")
-            unban_embed.add_field(name="Unbanned User",
-                                  value=f"<@{member_id}> has been unbanned from **{message.guild.name}** by {message.author.mention}")
+                                 icon_url=f"{self.client.user.display_avatar}")
+            unban_embed.add_field(name="Unbanned User", value=f"<@{member_id}>")
+            unban_embed.add_field(name="Unbanned By", value=f"{message.author.mention}")
             unban_embed.set_thumbnail(url=f"{member.display_avatar}")
 
-            await channel.send(embed=unban_embed)
+            await channel.send(embed=unban_embed)                
 
         except Exception as err:
             print(f"Unexpected {err=}, {type(err)=}")
+            await message.channel.send("No user found!")
             raise
 
 
